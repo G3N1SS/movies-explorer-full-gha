be99045ac9f2feda60ff9f596eea85fddb9831ff
Areg <areg.sarkisyan.99@mail.ru>
2024-11-22 15:04:36 +0300

    final commit

--- frontend/src/components/Intro.jsx
+++ frontend/src/components/Intro.jsx
@@ -5,8 +5,8 @@ function Intro(){
     <section className="intro">
       <div className="container container_intro">
         <div className="intro__info">
-          <h1 className="intro__title">Учебный&nbsp;проект студента факультета<br/>Веб-разработки.</h1>
-          <p className="intro__caption">Листайте ниже, чтобы узнать больше про этот проект и его создателя.</p>
+          <h1 className="intro__title">Учебный&nbsp;проект студентов факультета<br/>Программной инженерии</h1>
+          <p className="intro__caption">Листайте ниже, чтобы узнать больше про этот проект и его создателей.</p>
           <a className="intro__learn-more button" href='#about'>Узнать больше</a>
         </div>
         <img src={webglobus} alt="globus" className="intro__image"/>
